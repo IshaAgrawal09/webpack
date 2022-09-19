996692e71285498d66d6270fe5844aa6c87e2f11
IshaAgrawal09 <ishaagrawal@cedcoss.com>
2022-09-19 13:13:29 +0530

    Change Entry and output file

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,11 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-
+  entry: path.resolve(__dirname, "src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
   devServer: {
     static: {
       directory: path.resolve(__dirname, "dist"),
